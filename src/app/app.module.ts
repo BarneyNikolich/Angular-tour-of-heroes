d3cbf83d85aab4b1d8c32544a4dffff13514eeff
Barney Nikolich <Barney.Nikolich@lhasalimited.org>
2017-08-30 17:22:41 +0100

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,37 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero1-service';
-
-RouterModule.forRoot([
-  {
-    path: 'heroes',
-    component: HeroesComponent
-  }
-])
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+      RouterModule.forRoot([
+          {
+              path: '',
+              redirectTo: '/dashboard',
+              pathMatch: 'full'
+          },
+          {
+              path: 'heroes',
+              component: HeroesComponent
+          },
+          {
+              path: 'dashboard',
+              component: DashboardComponent
+          },
+          {
+              path: 'detail:id',
+              component: HeroDetailComponent
+          }
+      ])
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit  } from '@angular/core';
+import { HeroService } from './hero1-service';
+import { Hero } from './hero';
+
+
+@Component({
+    selector: 'my-dashboard',
+    templateUrl: './dashboard.component.html'
+})
+
+export class DashboardComponent {
+
+    heroes: Hero[] = [];
+
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit(): void {
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.splice(0, 5));
+    }
+}
