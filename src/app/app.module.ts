7cd5afba3ecd9f7b134ac3b82d17e6397a8f6742
Barney Nikolich <Barney.Nikolich@lhasalimited.org>
2017-08-30 15:22:48 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero1-service';
+
+RouterModule.forRoot([
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+])
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
